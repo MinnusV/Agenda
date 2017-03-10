c4356dfdcdf5c5fd122f85cf3b47155659df717b
Erick Costa <erick.costa@virtus.ufcg.edu.br>
2017-03-10 13:41:12 -0300

    Agenda Android

---
+++ Agenda/app/src/main/java/br/com/costa/agenda/StudentsInsertActivity.java
@@ -0,0 +1,18 @@
+package br.com.costa.agenda;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class StudentsInsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_insert);
+    }
+
+}
---
+++ Agenda/app/src/main/java/br/com/costa/agenda/StudentsListActivity.java
@@ -0,0 +1,30 @@
+package br.com.costa.agenda;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class StudentsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //Carregando configurações do AppCompatActivity para o onCreate
+        super.onCreate(savedInstanceState);
+        //Adicionando o xml ao conteúdo da tela
+        setContentView(R.layout.activity_students_list);
+
+        //Inserindo alunos na listview definida no xml
+        String[] studends = {"Erick", "John", "Fidelis ", "Costa", "João"};
+        ListView studeList = (ListView) findViewById(R.id.studentsListView);
+        ArrayAdapter<String> studentsListViewAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, studends);
+        studeList.setAdapter(studentsListViewAdapter);
+    }
+}
